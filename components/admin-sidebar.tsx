d8b9d7e6b6bf915c75a83b391518ee3dc8f1d027
rishan-ch <rishanchhunju@gmail.com>
2026-02-27 12:42:22 +0545

    applicant crud

--- components/admin-sidebar.tsx
+++ components/admin-sidebar.tsx
@@ -34,9 +34,9 @@ const sidebarItems: SidebarItem[] = [
     title: "Talents",
     icon: Briefcase,
     children: [
-      { title: "Applicant Portal", icon: Plus, href: "/admin/add-applicant" },
-      { title: "Applicant List", icon: Plus, href: "/admin/successful-applicants" },
-      { title: "Applicant Edit", icon: Plus, href: "/admin/edit-applicant" },
+      { title: "Applicant Portal", icon: Plus, href: "/admin/applicants/add" },
+      { title: "Applicant List", icon: Plus, href: "/admin/applicants" },
+      { title: "Applicant Edit", icon: Plus, href: "/admin/applicants/edit" },
     ],
   },
   {
